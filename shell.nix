a47a211f5ac599279d08d5947a3416299faf157e
asd <aravindhv10@gmail.com>
2025-09-29 22:26:25 +0530

    Got it working on wgpu

--- shell.nix
+++ shell.nix
@@ -4,16 +4,15 @@
     cargo
     fish
     rustc
+    vulkan-tools
+    vulkan-loader
+    vulkan-headers
   ];
 in
   (pkgs.buildFHSEnv {
     name = "simple-x11-env";
-
     targetPkgs = pkgs: mylist;
-
     multiPkgs = pkgs: mylist;
-
     runScript = "fish";
   })
-
 .env
