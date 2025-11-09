9aa7570cc6e09b7534a43f5612edda965880cd3f
Your Name <you@example.com>
2025-11-09 21:41:01 +0530

    More additions to rust code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use aotinductor::ModelPackage;
+use tch::CModule;
 use tch::Device;
 use tch::Kind;
 use tch::Tensor;
@@ -8,6 +8,13 @@ fn main() {
     let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
     let t = t * 2;
     t.print();
+
+    let model = CModule::load("./out.pt")
+        .context("Failed to load TorchScript model. Check the file path and LibTorch setup.")?;
+
+    let device = Device::cuda_if_available();
+    model.to_device(device);
+    println!("Model loaded successfully and moved to {:?}", device);
 
     println!("asd")
 }
