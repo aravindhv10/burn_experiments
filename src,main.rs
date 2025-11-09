18534457c133af911d54a5d86491b2507a46d9e7
Your Name <you@example.com>
2025-11-09 21:51:50 +0530

    More work on the rust code

--- src,main.rs
+++ src,main.rs
@@ -3,17 +3,16 @@ use tch::Device;
 use tch::Kind;
 use tch::Tensor;
 
-fn main() {
+fn main() -> Result<(), Error> {
 
     let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
     let t = t * 2;
     t.print();
 
-    let model = CModule::load("./out.pt")
-        .context("Failed to load TorchScript model. Check the file path and LibTorch setup.")?;
-
     let device = Device::cuda_if_available();
-    model.to_device(device);
+    let model = CModule::load_on_device("./out.pt", device)?;
+
+    model.to_device_(device);
     println!("Model loaded successfully and moved to {:?}", device);
 
     println!("asd")
