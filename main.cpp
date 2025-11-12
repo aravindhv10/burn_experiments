423c6a2de56a80a6243271a4700f39e0cfdec9af
asd <aravindhv10@gmail.com>
2025-11-12 08:41:11 +0530

    Added fully working c++ inference code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include <torch/csrc/inductor/aoti_package/model_package_loader.h>
+#include <torch/torch.h>
+
+int main() {
+  c10::InferenceMode mode;
+
+  torch::inductor::AOTIModelPackageLoader loader("out.pt2");
+  std::vector<torch::Tensor> inputs = {torch::randn({4, 100}, at::kCPU)};
+  std::vector<torch::Tensor> outputs = loader.run(inputs);
+  std::cout << "Result from the first inference:" << std::endl;
+  std::cout << outputs[0] << std::endl;
+
+  return 0;
+}
