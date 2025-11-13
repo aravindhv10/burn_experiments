aa5a5b4f294751b55fdfe94d982aad123c2ca20b
asd <aravindhv10@gmail.com>
2025-11-13 21:11:09 +0530

    Major improvements on c++ side

--- main.cpp
+++ main.cpp
@@ -4,14 +4,22 @@
 #include <torch/csrc/inductor/aoti_package/model_package_loader.h>
 #include <torch/torch.h>
 
-int main() {
-  c10::InferenceMode mode;
+extern "C" {
+torch::Tensor myfun(torch::Tensor in) {
+  static c10::InferenceMode mode;
+  static torch::inductor::AOTIModelPackageLoader loader("out.pt2");
 
-  torch::inductor::AOTIModelPackageLoader loader("out.pt2");
-  std::vector<torch::Tensor> inputs = {torch::randn({4, 100}, at::kCPU)};
+  // std::vector<torch::Tensor> inputs = {torch::randn({4, 100}, at::kCPU)};
+  std::vector<torch::Tensor> inputs = {in};
   std::vector<torch::Tensor> outputs = loader.run(inputs);
-  std::cout << "Result from the first inference:" << std::endl;
+  // std::cout << "Result from the first inference:" << std::endl;
   std::cout << outputs[0] << std::endl;
+  return outputs[0];
+}
+}
 
+int main() {
+  // std::vector<torch::Tensor> inputs = {torch::randn({4, 100}, at::kCPU)};
+  std::cout <<  myfun(torch::randn({4, 100}, at::kCPU)) << std::endl;
   return 0;
 }
