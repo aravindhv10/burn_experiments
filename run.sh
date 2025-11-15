bd4b4e334d03f09d88e5a802a9c06d2af826d4e8
asd <aravindhv10@gmail.com>
2025-11-15 14:38:29 +0530

    Added torch dependencies

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
+# export CXXFLAGS='-I/usr/include/torch/csrc/api/include/'
+# export CXXFLAGS='-laoti_custom_ops -lbackend_with_compiler -lc10 -lgomp-98b21ff3 -ljitbackend_test -lnnapi_backend -lshm -ltorch -ltorch_cpu -ltorch_global_deps -ltorch_python -ltorchbind_test'
 cargo run
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,12 @@
+#include "stdio.h"
 #include <iostream>
 #include <vector>
 
 #include <torch/torch.h>
 #include <torch/csrc/inductor/aoti_package/model_package_loader.h>
-
 extern "C" {
 int torchmain() {
-    c10::InferenceMode mode;
-
-    torch::inductor::AOTIModelPackageLoader loader("out.pt2");
-    // Assume running on CUDA
-    std::vector<torch::Tensor> inputs = {torch::randn({4, 100}, at::kCPU)};
-    std::vector<torch::Tensor> outputs = loader.run(inputs);
-    std::cout << "Result from the first inference:"<< std::endl;
-    std::cout << outputs[0] << std::endl;
+  printf("Working...\n");
 
     return 0;
 }
