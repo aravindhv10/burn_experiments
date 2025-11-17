eca04a7189b88b24b37da10455835f4eb9212296
asd <aravindhv10@gmail.com>
2025-11-17 22:52:44 +0530

    Removed script to build image

--- host.image_names.sh
+++ host.image_names.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 IMAGE_NAME='debtestrustzshhelixpytorch'
 
-BUILD_CONTAINER () {
-    CMD='sudo -A docker'
-    which buildah && CMD='buildah'
-
-    ${CMD} build \
-        -t "${IMAGE_NAME}" \
-        -f "./Dockerfile" ;
-}
-
 RUN_CONTAINER () {
     CMD='sudo -A docker'
     which podman && CMD='podman'
