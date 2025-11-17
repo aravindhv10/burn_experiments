2a8528c5e387a976d11206a908dcf4a6b2d5596b
Your Name <you@example.com>
2025-11-17 19:58:06 +0530

    Added more refinements and switched to using clang

--- compile_g++.sh
+++ compile_g++.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 mkdir -pv -- './lib/' './tmp/'
-clang++ \
+
+bear -- \
+    clang++ \
     './src/main.cpp' -o './tmp/main.o' \
     -c -fPIC \
     '-I/usr/include/torch/csrc/api/include/' \
 ;
 
-clang++ \
-    './tmp.cpp' -o './tmp.exe' \
-    './tmp/main.o' \
-    -ltorch \
-    -ltorch_cpu \
-    -lc10 \
-;
+# clang++ \
+#     './tmp.cpp' -o './tmp.exe' \
+#     './tmp/main.o' \
+#     -ltorch \
+#     -ltorch_cpu \
+#     -lc10 \
+# ;
     # -laoti_custom_ops \
     # -lbackend_with_compiler \
     # -lgomp-98b21ff3 \
