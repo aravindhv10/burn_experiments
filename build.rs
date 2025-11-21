532a4ce6063cd4bdba16140c027ee5fab82b6c05
asd <aravindhv10@gmail.com>
2025-11-21 12:24:00 +0530

    More refinements to scripts

--- build.rs
+++ build.rs
@@ -1,29 +1,3 @@
 fn main() {
     println!("cargo:rustc-link-arg=-lmytorch");
-
-    // let library_path = std::path::Path::new("/usr/include/torch/csrc/api/include/");
-
-    // cc::Build::new()
-    //     .cpp(true)
-    //     .file("src/main.cpp")
-    //     .include(library_path)
-    //     .flag("-Ofast")
-    //     .flag("-mtune=native")
-    //     .flag("-march=native")
-    //     .compile("main");
-
-    // println!("cargo:rustc-link-arg=-laoti_custom_ops");
-    // println!("cargo:rustc-link-arg=-lbackend_with_compiler");
-    // println!("cargo:rustc-link-arg=-lc10");
-    // println!("cargo:rustc-link-arg=-lgomp-98b21ff3");
-    // println!("cargo:rustc-link-arg=-ljitbackend_test");
-    // println!("cargo:rustc-link-arg=-lmytorch");
-    // println!("cargo:rustc-link-arg=-lnnapi_backend");
-    // println!("cargo:rustc-link-arg=-lshm");
-    // println!("cargo:rustc-link-arg=-ltorch");
-    // println!("cargo:rustc-link-arg=-ltorchbind_test");
-    // println!("cargo:rustc-link-arg=-ltorch_cpu");
-    // println!("cargo:rustc-link-arg=-ltorch_global_deps");
-    // println!("cargo:rustc-link-arg=-ltorch_python");
-    // println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
 }
