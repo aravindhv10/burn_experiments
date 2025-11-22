25f8dc02cdc172ca2909662cea2635b3482c787f
asd <aravindhv10@gmail.com>
2025-11-22 15:57:07 +0530

    Fixed bug in compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 H="$(cat './model_input.pt2' './compile.py' | sha512sum | cut -d ' ' -f1)"
 test -e "${H}.pt2" && exit '0'
-./compile.sh './model_input.pt2' "./${H}.pt2"
+./compile.py './model_input.pt2' "./${H}.pt2"
 ln -vfs -- "./${H}.pt2" './model_output.pt2'
 exit '0'
