23b468bc4176fd404aa291b20f47955f72945fdb
Your Name <you@example.com>
2025-12-03 09:43:02 +0000

    Fixing bugs

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 cd "$(dirname -- "${0}")"
 make -j install
 export RUSTFLAGS="-C target-cpu=native"
-export RUSTFLAGS="ROCR_VISIBLE_DEVICES=1"
+export ROCR_VISIBLE_DEVICES=1
 cargo run --bin infer-server --release
