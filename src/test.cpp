f36e145697db164c59f0d8da1579169916674581
Your Name <you@example.com>
2025-12-04 14:00:23 +0000

    Added more debugging stuff

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,8 @@ int main(){
   arg_input in;
   arg_output out;
   mylibtorchinfer(/*arg_input *in =*/ &in, /*unsigned int const batch_size =*/ 1, /*arg_output *out =*/ &out);
+  for(int i = 0; i< NUM_CLASSES; ++i){printf("%lf, ",out.val[i]);}
+  printf("\n");
   
   return 0;
 }
