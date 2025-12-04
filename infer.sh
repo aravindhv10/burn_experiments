cb77eca3c29a2ce39567ebdae626b486291619f3
Your Name <you@example.com>
2025-12-04 14:29:16 +0000

    More cleanups

--- infer.sh
+++ infer.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
-make -j all
 export RUSTFLAGS="-C target-cpu=native"
 cargo run --bin infer-client --release
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
-make -j install
 export RUSTFLAGS="-C target-cpu=native"
 export ROCR_VISIBLE_DEVICES=1
 cargo run --bin infer-server --release
