c71bb323ba923961d489227473eb9fee7e2bbd43
Your Name <you@example.com>
2025-12-04 14:48:18 +0000

    Added script to do proper bindgen

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 cd "$(dirname -- "${0}")"
 export RUSTFLAGS="-C target-cpu=native"
 # export ROCR_VISIBLE_DEVICES=1
+bindgen './src/export.hpp' > './src/export.rs'
 cargo run --bin infer-server --release
