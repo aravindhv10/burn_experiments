df892c7f05f3048ac79ceb84c1917b2fcaa217b3
Your Name <you@example.com>
2025-12-04 13:48:37 +0000

    Fixed typo in c++

--- src/test.cpp
+++ src/test.cpp
@@ -2,5 +2,6 @@
 int main(){
   arg_input in;
   arg_output out;
-  mylibtorchinfer(/*arg_input *in =*/ &in, /*unsigned int const batch_size =*/ 1, /*arg_output *out =*/ &out)
+  mylibtorchinfer(/*arg_input *in =*/ &in, /*unsigned int const batch_size =*/ 1, /*arg_output *out =*/ &out);
+  return 0;
 }
