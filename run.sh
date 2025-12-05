b2cf881850b92b60c9d16ef335fc9b60ac6f0b60
Your Name <you@example.com>
2025-12-05 02:07:12 +0000

    More refinements

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
 export RUSTFLAGS="-C target-cpu=native"
-# export ROCR_VISIBLE_DEVICES=1
+export ROCR_VISIBLE_DEVICES=0
 bindgen './src/export.hpp' > './src/export.rs'
 cargo run --bin infer-server --release
