6f906c56a10df7303b30d0502676f8ca4ad724f1
Your Name <you@example.com>
2025-12-05 02:50:29 +0000

    More refinements for docker scripts

--- image_run.sh
+++ image_run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
 
+mkdir -pv -- "${HOME}/BUILD"
+
 IMAGE_NAME='debtestrustzshhelixpytorch2'
 
 RUN_CONTAINER () {
@@ -11,8 +13,11 @@ RUN_CONTAINER () {
         '--device' '/dev/kfd' \
         '--device' '/dev/dri' \
         '--security-opt' 'seccomp=unconfined' \
+        --mount 'type=tmpfs,destination=/data/TMPFS,tmpfs-size=137438953472' \
         -v "$(realpath .):/data/source" \
-        -v "CACHE:/data/build" \
+        -v "${HOME}/BUILD:/data/build" \
+        -v "CACHE:/usr/local/cargo/registry" \
+        -v "CACHE:/root/.cache" \
         "${IMAGE_NAME}" zsh \
     ;
 }
