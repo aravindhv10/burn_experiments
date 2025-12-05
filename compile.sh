67d5535484a968499a4034c56bb343ca68ece26f
Your Name <you@example.com>
2025-12-05 02:16:09 +0000

    More refinements in build scripts

--- compile.sh
+++ compile.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
 SRC="$(realpath .)"
-mkdir -pv -- '../build'
-cd '../build'
-BLD="$(realpath .)"
+BLD="${SRC}/../build"
+mkdir -pv -- "${BLD}"
+
+cd "${BLD}"
 cmake "${SRC}"
 make -j4
 make install
+
+cd "${SRC}"
+export RUSTFLAGS="-C target-cpu=native"
+bindgen './src/export.hpp' > './src/export.rs'
+cargo build --bin infer-server --release
+cp -vf -- 'target/release/infer-server' "${BLD}/"
+
 cd "${SRC}"
 H="$(sha512sum ./main.py | cut -d ' ' -f1)"
 mkdir -pv -- "${BLD}/${H}"
-test -e "${BLD}/${H}/model.pt2" && exit '0'
-./main.py "${BLD}/${H}/model.pt2"
+test -e "${BLD}/${H}/model.pt2" || ./main.py "${BLD}/${H}/model.pt2"
 ln -vfs -- "${BLD}/${H}/model.pt2" "${BLD}/model.pt2"
+
 exit '0'
--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
-export RUSTFLAGS="-C target-cpu=native"
+SRC="$(realpath .)"
+BLD="${SRC}/../build"
+mkdir -pv -- "${BLD}"
+cd "${BLD}"
+
 export ROCR_VISIBLE_DEVICES=0
-bindgen './src/export.hpp' > './src/export.rs'
-cargo run --bin infer-server --release
+./infer-server
