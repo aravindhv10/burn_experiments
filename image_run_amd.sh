f0d22d2dcf827aaae2b827e1e59bb1cb88934344
Your Name <you@example.com>
2025-12-13 03:21:10 +0000

    Added script for running container on cpu

--- image_run_amd.sh
+++ image_run_amd.sh
@@ -10,12 +10,15 @@ RUN_CONTAINER () {
     which podman && CMD='podman'
     ${CMD} run \
         -it --rm \
+        '--device' '/dev/kfd' \
+        '--device' '/dev/dri' \
         '--security-opt' 'seccomp=unconfined' \
         --mount 'type=tmpfs,destination=/data/TMPFS,tmpfs-size=137438953472' \
         -v "$(realpath .):/data/source" \
         -v "${HOME}/BUILD:/data/build" \
         -v "CACHE:/usr/local/cargo/registry" \
         -v "CACHE:/root/.cache" \
+        -e 'ROCR_VISIBLE_DEVICES=0' \
         "${IMAGE_NAME}" zsh \
     ;
 }
