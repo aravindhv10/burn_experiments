9ee8fe1c3dfd52cf170b0dd7714ecb9d15ff2e9a
Your Name <you@example.com>
2025-12-18 09:53:13 +0000

    Added python files to compile

--- compile.py
+++ compile.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-from main import compile_model_to_aot_inductor
+from main import produce_model_ep
+from compile_2_trt import compile_EP_2_optimized_targer
 import sys
 
 if __name__ == "__main__":
-    compile_model_to_aot_inductor(sys.argv[1], sys.argv[2])
+    produce_model_ep(path_file_output_model_ep=sys.argv[1])
+    compile_EP_2_optimized_targer(
+        path_file_input_EP_pt2=sys.argv[1],
+        path_file_output_compiled_pt2=sys.argv[2],
+    )
