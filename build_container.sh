b59e08458739c6c36f77ee26f0cf9f94b3482bc5
Your Name <you@example.com>
2025-12-20 10:21:09 +0000

    Added compile_2_trt to docker build hash checkint

--- build_container.sh
+++ build_container.sh
@@ -3,7 +3,7 @@ cd "$('dirname' -- "${0}")"
 IMAGE_NAME='7_rust_libtorch_demo'
 mkdir -pv -- './build'
 
-H="$(cat './CMakeLists.txt' './Cargo.toml' './Dockerfile' './build.rs' './compile.py' './compile.sh' './curl.sh' './infer.proto' './infer.sh' './main.py' './run.sh' './src/client.rs' './src/export.hpp' './src/main.cpp' './src/main.hpp' './src/main.rs' './src/model.rs' './src/mylib.rs' './src/test.cpp' | sha512sum | cut -d ' ' -f1)"
+H="$(cat './CMakeLists.txt' './Cargo.toml' './Dockerfile' './build.rs' './compile.py' './compile_2_trt.py' './compile.sh' './curl.sh' './infer.proto' './infer.sh' './main.py' './run.sh' './src/client.rs' './src/export.hpp' './src/main.cpp' './src/main.hpp' './src/main.rs' './src/model.rs' './src/mylib.rs' './src/test.cpp' | sha512sum | cut -d ' ' -f1)"
 
 test -e "./build/${H}" && exit '0'
 CMD='sudo -A docker'
